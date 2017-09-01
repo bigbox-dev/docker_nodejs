a7a6d014ab91640070775abcd84ae122f19028d1
Dmitry Kochetov <d.kochetov@initlab.ru>
2017-09-01 15:13:36 +0300

    fix

--- run.sh
+++ run.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-domains=$(ls | column -t)
-for domain in $domains; do
-  port=$(sed "s/^0*//" $domain/html/frontend/frontend.port)
-  sh -c "PORT=$port pm2 start $domain/html/frontend/server.js  --name "$domain"
+domains=$(ls /app/www/ | xargs -n1)  && \
+echo "domains=$domains"  && \
+start=$(for domain in $domains; do
+  echo "domain=$domain"
+  port=$(sed "s/^0*//" /app/www/$domain/current_build/frontend/frontend.port)
+  echo "$port"
+  PORT=$port cd /app/www/$domain/current_build/frontend && pm2 start server.js  --name "$domain"
+
+done)
+
+echo "$start"
+
+bash
+echo "6"
+exec bash
+echo "7"
+while true; do
+sleep 3
+echo "sleep"
 done
