8d994b516ed2317ef3bcba26c16493d2f7acded0
Dmitry Kochetov <d.kochetov@initlab.ru>
2017-09-04 09:51:46 +0300

    fix start script

--- run.sh
+++ run.sh
@@ -6,17 +6,12 @@ start=$(for domain in $domains; do
   echo "domain=$domain"
   port=$(sed "s/^0*//" /app/www/$domain/current_build/frontend/frontend.port)
   echo "$port"
-  PORT=$port cd /app/www/$domain/current_build/frontend && pm2 start server.js  --name "$domain"
-
+  cd /app/www/$domain/current_build/frontend
+  PORT=$port pm2 start server.js  --name "$domain"
 done)
 
 echo "$start"
 
-bash
-echo "6"
-exec bash
-echo "7"
 while true; do
-sleep 3
-echo "sleep"
+sleep 10
 done
